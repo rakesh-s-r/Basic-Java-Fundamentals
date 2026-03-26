3f694324c59f91c779b3ec5a322743a66c6ff650
Rakesh SR <rakeshsr1998@gmail.com>
2026-03-26 23:01:18 +0530

    Added hashmaps

--- OOPS/Advanced/HashMaps.java
+++ OOPS/Advanced/HashMaps.java
@@ -7,10 +7,19 @@ public class HashMaps {
     public static void main(String[] args) {
         Map<Integer, Integer> map = new HashMap<>();
         map.put(1, 10);
-        System.out.println(map.size());
+        map.putIfAbsent(2, 2);
+        System.out.println("Size of the map is " + map.size());
 
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
             System.out.println(entry.getKey());
+        }
+
+        for (Integer entry : map.keySet()) {
+            System.out.println(entry);
+        }
+
+        for (Integer entry : map.values()) {
+            System.out.println(entry);
         }
     }
 }
